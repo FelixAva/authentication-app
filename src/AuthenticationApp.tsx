bc6a74853e94066a57e53659eed45fb3983c56e3
Felix <felavalos@gmail.com>
2024-08-20 09:45:32 -0600

    Folders system
    Create folder src
    Move App to src folder
    Rename App.tsx to AuthenticationApp.tsx
    Create presentation folder
    Create components and screens folders in presentation

---
+++ src/AuthenticationApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AuthenticationApp = () => {
+  return (
+    <View>
+      <Text>AuthenticationApp</Text>
+    </View>
+  );
+}
+
+export default AuthenticationApp;
