faa95101acd583ed877d1d077f29720877db44fc
Felix <felavalos@gmail.com>
2024-09-19 19:03:27 -0600

    Delete CLI App Version Create Expo App

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import AuthenticationApp from './src/AuthenticationApp';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => AuthenticationApp);
