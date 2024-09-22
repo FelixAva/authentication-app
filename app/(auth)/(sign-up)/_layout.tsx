8506d390833b2916b5474e97ac2b9627e8b28f0b
Felix <felavalos@gmail.com>
2024-09-22 14:13:39 -0600

    Create sign-in and sign-up routes
    Sign In:
    Create sign-in folder in app/(auth)/
    Create index.tsx and _layout.tsx files in .../sign.in/
    
    Sign Up:
    Create sign-up folder in app/(auth)/
    Create index.tsx and _layout.tsx files in .../sign-up/

---
+++ app/(auth)/(sign-up)/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SignUpLayout() {
+  return (
+    <View>
+      <Text>SignUpLayout</Text>
+    </View>
+  );
+}
