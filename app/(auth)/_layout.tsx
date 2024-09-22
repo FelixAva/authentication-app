7fb05eb8a1f2022e73eef45f078151991f50d54b
Felix <felavalos@gmail.com>
2024-09-22 14:10:43 -0600

    Create auth folder
    Create (auth) folder to save the sign-in and sign-up screens
    Create a _layout.tsx file in app/(auth)

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AuthLayout() {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  );
+}
