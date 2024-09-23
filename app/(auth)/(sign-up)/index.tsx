4f4d20589ce6e509904c26e0b627c96768d087e4
Felix <felavalos@gmail.com>
2024-09-23 11:35:46 -0600

    Sign Up react hook form
    Add the react hoook to sign-up/index
    Move the InputTexts components to the hookForm Controller components
    Create the rules to the field
    Create the rule to confirmPassword, rule to have the same value of password field
    Create function to submit the data

--- app/(auth)/(sign-up)/index.tsx
+++ app/(auth)/(sign-up)/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
 import {
   Button,
@@ -9,40 +10,138 @@ import {
 } from '../../../components';
 
 export default function SignUp() {
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      firstName: '',
+      secondName: '',
+      password: '',
+      confirmPassword: '',
+    }
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <ContentSafeArea>
       <FormContainer>
-        <InputText
-          label='First name'
-          inputProps={{
-            placeholder: 'Type your first name'
+        <Controller
+          control={ control }
+          rules={{
+            required: {
+              value: true,
+              message: 'This field is required'
+            },
+            maxLength: {
+              value: 12,
+              message: 'Max length is 12'
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='First name'
+              inputProps={{
+                placeholder: 'Type your first name',
+                onChange: onChange,
+                onBlur: onBlur,
+                value: value
+              }}
+              error={ errors.firstName?.message }
+            />
+          )}
+          name='firstName'
         />
 
-        <InputText
-          label='Second name'
-          inputProps={{
-            placeholder: 'Type your second name'
+        <Controller
+          control={ control }
+          rules={{
+            required: {
+              value: true,
+              message: 'This field is required'
+            },
+            maxLength: {
+              value: 12,
+              message: 'Max length is 12'
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='Second name'
+              inputProps={{
+                placeholder: 'Type your second name',
+                onChange: onChange,
+                onBlur: onBlur,
+                value: value
+              }}
+              error={ errors.secondName?.message }
+            />
+          )}
+          name='secondName'
         />
 
-        <InputText
-          label='Password'
-          inputProps={{
-            placeholder: 'Type your password'
+        <Controller
+          control={ control }
+          rules={{
+            required: {
+              value: true,
+              message: 'This field is required'
+            },
+            minLength: {
+              value: 4,
+              message: 'Min length is 4'
+            },
+            maxLength: {
+              value: 12,
+              message: 'Max length is 12'
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='Password'
+              inputProps={{
+                placeholder: 'Type your password',
+                onChange: onChange,
+                onBlur: onBlur,
+                value: value
+              }}
+              error={ errors.password?.message }
+            />
+          )}
+          name='password'
         />
 
-        <InputText
-          label='Confirm password'
-          inputProps={{
-            placeholder: 'Type your password again'
+        <Controller
+          control={ control }
+          rules={{
+            validate: ( val: string ) => {
+              if ( watch('password') != val ) {
+                return 'The passwords do not match'
+              }
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='Confirm password'
+              inputProps={{
+                placeholder: 'Type your password again',
+                onChange: onChange,
+                onBlur: onBlur,
+                value: value
+              }}
+              error={ errors.confirmPassword?.message }
+            />
+          )}
+          name='confirmPassword'
         />
 
         <Button
           title='Sign Up'
-          action={ () => console.log('Sign Up') }
+          action={ handleSubmit( onSubmit ) }
           customStyles={{ width: 350 }}
         />
       </FormContainer>
