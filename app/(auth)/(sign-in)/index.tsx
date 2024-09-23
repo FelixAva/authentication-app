8a173f68c7008fe19032f2466d0fd0eaed734f31
Felix <felavalos@gmail.com>
2024-09-23 10:51:55 -0600

    Sign in form hook
    Add hook useform to the sign in
    Create controller component (useForm component), and render the InputText component in
    Send hook errors as InputText properties
    Create a sumbit function to show on console the inputs values

--- app/(auth)/(sign-in)/index.tsx
+++ app/(auth)/(sign-in)/index.tsx
@@ -25,23 +25,63 @@ export default function SignIn() {
     <View style={ styles.container }>
 
       <FormContainer>
-        <InputText
-          label='User'
-          inputProps={{
-            placeholder: 'Type your user name'
+        <Controller
+          control={ control }
+          rules={{
+            required: {
+              value: true,
+              message: 'This field is required'
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='User'
+              inputProps={{
+                placeholder: 'Type your user name',
+                onBlur: onBlur,
+                onChange: onChange,
+                value: value
+              }}
+              error={ errors.user?.message }
+            />
+          )}
+          name='user'
         />
 
-        <InputText
-          label='Password'
-          inputProps={{
-            placeholder: 'Type your password'
+        <Controller
+          control={ control }
+          rules={{
+            required: {
+              value: true,
+              message: 'This field is required'
+            },
+            minLength: {
+              value: 4,
+              message: 'Min length is 4'
+            },
+            maxLength: {
+              value: 12,
+              message: 'Max length is 12'
+            }
           }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <InputText
+              label='Password'
+              inputProps={{
+                placeholder: 'Type your password',
+                onBlur: onBlur,
+                onChange: onChange,
+                value: value
+              }}
+              error={ errors.password?.message }
+            />
+          )}
+          name='password'
         />
 
         <Button
           title='Sign In'
-          action={ () => console.log('Sign In') }
+          action={ handleSubmit( onSubmit ) }
           customStyles={{ width: 350 }}
         />
       </FormContainer>
