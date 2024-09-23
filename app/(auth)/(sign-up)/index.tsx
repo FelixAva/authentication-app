867de8fa64a5caf94a39f393e3abb933fed96f0b
Felix <felavalos@gmail.com>
2024-09-22 23:12:20 -0600

    Create Sign Up form

--- app/(auth)/(sign-up)/index.tsx
+++ app/(auth)/(sign-up)/index.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { Link } from '../../../components';
+import { View, StyleSheet } from 'react-native';
+
+import { globalStyles } from '../../../constants';
+import { Button, FormContainer, InputText, Link } from '../../../components';
 
 export default function SignUp() {
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={ styles.container } collapsable>
+
+      <FormContainer>
+        <InputText
+          label='First name'
+          inputProps={{
+            placeholder: 'Type your first name'
+          }}
+        />
+
+        <InputText
+          label='Second name'
+          inputProps={{
+            placeholder: 'Type your second name'
+          }}
+        />
+
+        <InputText
+          label='Password'
+          inputProps={{
+            placeholder: 'Type your password'
+          }}
+        />
+
+        <InputText
+          label='Confirm password'
+          inputProps={{
+            placeholder: 'Type your password again'
+          }}
+        />
+
+        <Button
+          title='Sign Up'
+          action={ () => console.log('Sign Up') }
+          customStyles={{ width: 350 }}
+        />
+      </FormContainer>
+
       <Link
         label="Do you have an account?"
         route='sign-in'
@@ -13,3 +51,11 @@ export default function SignUp() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...globalStyles.container,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  }
+});
