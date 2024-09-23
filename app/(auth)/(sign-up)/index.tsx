02fdda611369dc4d99c16b319879abb05c982ac5
Felix <felavalos@gmail.com>
2024-09-22 23:55:29 -0600

    Sign Up Keyboard adapt to the inputs
    Adapt the keyboard to don't block the inputs
    Create conditional style when the keyboard is shown and hiden

--- app/(auth)/(sign-up)/index.tsx
+++ app/(auth)/(sign-up)/index.tsx
@@ -1,54 +1,73 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  View,
+  ScrollView
+} from 'react-native';
 
 import { globalStyles } from '../../../constants';
 import { Button, FormContainer, InputText, Link } from '../../../components';
 
 export default function SignUp() {
+
+  const isKeyboardActive = useRef( false );
+
+  Keyboard.addListener('keyboardDidShow', () => isKeyboardActive.current = true );
+  Keyboard.addListener('keyboardDidHide', () => isKeyboardActive.current = false );
+
   return (
-    <View style={ styles.container } collapsable>
+    <KeyboardAvoidingView style={ styles.container }
+      behavior='position'
+    >
+      <TouchableWithoutFeedback onPress={ Platform.OS === 'web' ? null : Keyboard.dismiss } style={{ flex:1 }}>
+        <ScrollView contentContainerStyle={[ styles.content, isKeyboardActive ? { flex: 0 } : { flex: 1 } ]}>
+          <FormContainer>
+            <InputText
+              label='First name'
+              inputProps={{
+                placeholder: 'Type your first name'
+              }}
+            />
 
-      <FormContainer>
-        <InputText
-          label='First name'
-          inputProps={{
-            placeholder: 'Type your first name'
-          }}
-        />
+            <InputText
+              label='Second name'
+              inputProps={{
+                placeholder: 'Type your second name'
+              }}
+            />
 
-        <InputText
-          label='Second name'
-          inputProps={{
-            placeholder: 'Type your second name'
-          }}
-        />
+            <InputText
+              label='Password'
+              inputProps={{
+                placeholder: 'Type your password'
+              }}
+            />
 
-        <InputText
-          label='Password'
-          inputProps={{
-            placeholder: 'Type your password'
-          }}
-        />
+            <InputText
+              label='Confirm password'
+              inputProps={{
+                placeholder: 'Type your password again'
+              }}
+            />
 
-        <InputText
-          label='Confirm password'
-          inputProps={{
-            placeholder: 'Type your password again'
-          }}
-        />
+            <Button
+              title='Sign Up'
+              action={ () => console.log('Sign Up') }
+              customStyles={{ width: 350 }}
+            />
+          </FormContainer>
 
-        <Button
-          title='Sign Up'
-          action={ () => console.log('Sign Up') }
-          customStyles={{ width: 350 }}
-        />
-      </FormContainer>
-
-      <Link
-        label="Do you have an account?"
-        route='sign-in'
-      />
-    </View>
+          <Link
+            label="Do you have an account?"
+            route='sign-in'
+          />
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -56,6 +75,10 @@ const styles = StyleSheet.create({
   container: {
     ...globalStyles.container,
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-between',
+  },
+  content: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
   }
 });
