5d31d4a866de47e774bbca237efc05e37e842f32
Felix <felavalos@gmail.com>
2024-09-24 13:45:43 -0600

    Zustand store
    Create zustand store
    Create token and user states
    Create update and delete actions for both states

---
+++ store/useStore.ts
@@ -0,0 +1,35 @@
+import { create } from 'zustand';
+
+import { User } from '../interfaces/user';
+
+type State = {
+  token: string;
+  user: User;
+}
+
+type Action = {
+  updateToken: ( token: State['token'] ) => void;
+  deleteToken: () => void;
+
+  updateUser: ( user: State['user'] ) => void;
+  deleteUser: () => void;
+}
+
+const useUserStore = create<State & Action>( (set) => ({
+  token: '',
+  user: undefined,
+
+  updateToken: ( token ) => set(() => ({
+    token: token
+  })),
+  deleteToken: () => set(() => ({
+    token: ''
+  })),
+
+  updateUser: ( user ) => set(() => ({
+    user: user
+  })),
+  deleteUser: () => set(() => ({
+    user: undefined
+  }))
+}));
