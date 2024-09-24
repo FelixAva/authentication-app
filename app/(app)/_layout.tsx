a23bac898cb413487a34f764878af3cd20650c76
Felix <felavalos@gmail.com>
2024-09-24 14:36:20 -0600

    Redirect to auth if token is not valid

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,8 +1,28 @@
-import { Stack } from 'expo-router';
-import React from 'react';
-import { View, Text } from 'react-native';
+import { Redirect, Stack } from 'expo-router';
+import React, { useState, useEffect } from 'react';
+
+import { ScreenLoading } from '../../components';
+
+import useUserStore from '../../store/useStore';
 
 export default function AppLayout() {
+
+  const [isLoading, setIsLoading] = useState<boolean>( false );
+
+  const token = useUserStore( state => state.token );
+
+  useEffect(() => {
+    setIsLoading( true );
+
+    setTimeout(() => {
+      setIsLoading( false );
+    }, 1000 );
+  }, [])
+
+  if ( isLoading ) return <ScreenLoading />;
+
+  if ( !token ) return <Redirect href='/(auth)/' />
+
   return (
     <Stack
       screenOptions={{
