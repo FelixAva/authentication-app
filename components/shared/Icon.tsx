ad3508ff6c1620d4e3cc2ab77c750fbb9c3797ca
Felix <felavalos@gmail.com>
2024-09-27 13:39:39 -0600

    Create custom icon component. Icon library IONICONS

---
+++ components/shared/Icon.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import IonIcons from '@expo/vector-icons/Ionicons'; // Icon library import. IONICONS
+import { View, Text } from 'react-native';
+
+interface Props {
+  name: any;
+  size: number;
+  color: string;
+}
+
+export default function ComponentScreen({
+  name,
+  size = 24,
+  color = 'black'
+}: Props ) {
+  return (
+    <>
+      <IonIcons
+        name={ name }
+        size={ size }
+        color={ color }
+      />
+    </>
+  );
+}
